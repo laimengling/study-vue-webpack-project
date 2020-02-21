7607a7c4c0e07990292b43b4037a7e5bad0c1098
unknown <770669665@qq.com>
2020-02-21 23:50:26 +0800

    主页的页面修改

--- src/main.js
+++ src/main.js
@@ -2,13 +2,29 @@
 入口文件
 * */
 import Vue from 'vue';
-import "./lib/mui/css/mui.min.css";
-//按需导入Mint-UI中的组件
-import { Header } from 'mint-ui';
-Vue.component(Header.name, Header);
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
 
+import "./lib/mui/css/mui.min.css";
+import "./lib/mui/css/icons-extra.css";
+
+//按需导入Mint-UI中的组件
+import { Header,Swipe, SwipeItem } from 'mint-ui';
+Vue.component(Header.name, Header);
+//轮播图组件
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+//请求数据 vue-resource
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
 import app from './App.vue';
+
+//导入自己的router.js路由模块
+import router from './router';
+
 var vm = new Vue({
     el:"#app",
-    render:c=>c(app)
+    render:c=>c(app),
+    router
 })
